2ced1b343ff823ed86746a5b5a9943843369f279
towa <7078986+towa@users.noreply.github.com>
2023-01-18 12:45:12 +0100

    add faster entry

--- qgis_starter.py
+++ qgis_starter.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python
 
-from qgis.PyQt.QtWidgets import QInputDialog, QApplication
+from qgis.PyQt.QtWidgets import (
+    QApplication, QDialog, QVBoxLayout, QLineEdit, QCompleter, QMessageBox)
+from qgis.PyQt.QtGui import QFont
+from qgis.PyQt.QtCore import Qt
 from pathlib import Path
 import subprocess
 
 
 def start_qgis():
-    _ = QApplication([])
+    app = QApplication([])
+    font = QFont("Ubuntu", 15)
+    app.setFont(font)
+
     profile_path = Path.home() / Path(".local/share/QGIS/QGIS3/profiles")
     profiles = [
         path.name
         for path in profile_path.iterdir()
         if path.is_dir()
     ]
-    profile, entry = QInputDialog.getItem(
-        None,
-        "Profilauswahl",
-        "Profil zum starten auswählen",
-        profiles,
-        editable=False
-    )
-    if entry:
-        subprocess.run(["qgis", "--profile", profile])
+    profiles.sort(key=lambda x: x.lower())
+
+    completer = QCompleter(profiles, None)
+    completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+    completer.setModelSorting(QCompleter.CaseInsensitivelySortedModel)
+    completer.setCaseSensitivity(Qt.CaseInsensitive)
+    line_edit = QLineEdit()
+    line_edit.setCompleter(completer)
+    dialog = QDialog()
+    layout = QVBoxLayout()
+    layout.addWidget(line_edit)
+    dialog.setLayout(layout)
+    dialog.setWindowTitle("Choose QGIS Profile:")
+    line_edit.returnPressed.connect(dialog.accept)
+    dialog.exec_()
+
+    profile = line_edit.text()
+
+    if profile and profile not in profiles:
+        answer = QMessageBox.question(
+            None,
+            "Create new profile",
+            f"Profile {profile} does not exist. Do you want to create it?"
+        )
+        if answer == QMessageBox.No:
+            return
+
+    subprocess.run(["qgis", "--profile", profile])
 
 
 if __name__ == '__main__':
